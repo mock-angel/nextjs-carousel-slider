174f451b388bdf7c31369d12439030f8dd3bb473
mock-angel <mockangelbusiness@gmail.com>
2024-12-27 15:05:59 +0530

    added typing to icons

--- src/svgs/star-fav.tsx
+++ src/svgs/star-fav.tsx
@@ -1,4 +1,6 @@
-export const StarFavIcon = (props: { style?: any }) => {
+import { CSSProperties } from "react";
+
+export const StarFavIcon = (props: { style?: CSSProperties | undefined; }) => {
     return <>
         <svg width="800px" height="800px" viewBox="0 -0.5 21 21" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={props.style}>
 
--- src/svgs/three-dots.tsx
+++ src/svgs/three-dots.tsx
@@ -1,4 +1,6 @@
-export const ThreeDots = (props: { style?: any }) => {
+import { CSSProperties } from "react";
+
+export const ThreeDots = (props: { style?: CSSProperties | undefined; }) => {
     return <>
         <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
             width="800px" height="800px" viewBox="0 0 342.382 342.382" style={props.style}
