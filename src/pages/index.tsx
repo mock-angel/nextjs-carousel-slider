e4900e164effecd1a9abd98cfe3bfef9551455b1
mock-angel <mockangelbusiness@gmail.com>
2024-12-28 14:58:53 +0530

    get width of carousel slider element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { CarouselSliderItem } from "@/components/carousel-slider-item";
+import { Ref, RefObject, useEffect, useRef } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,11 +13,23 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+
+  const ref: RefObject<HTMLDivElement | null> = useRef<null | HTMLDivElement>(null);
+  useEffect(() => {
+    const carouselSliderWidth = ref?.current?.getBoundingClientRect().width;
+    console.log("Width of slider is: ", carouselSliderWidth);
+
+  }, [ref])
+
+
+
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <div className="flex flex-row gap-4 overflow-y-auto">
+      <div className="flex flex-row gap-4 overflow-y-auto"
+        ref={ref}
+      >
         <div><CarouselSliderItem /></div>
         <div><CarouselSliderItem /></div>
         <div><CarouselSliderItem /></div>
